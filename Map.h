487621bd3fb5501e0368b12b706b81c8c29e7ec7
suhan0906 <201405suhan@gmail.com>
2023-04-25 02:19:44 +0800

    Map class header file

---
+++ Map.h
@@ -0,0 +1,25 @@
+#ifndef MAP_H
+#define MAP_H
+#include <iostream>
+#include <string>
+#include "Station.h"
+using namespace std;
+
+class Map {
+private:
+    string mapChart[100][100];
+    Station mapPlayerLocation[7][7]; 
+    void Map::removeMal(int row, int col);
+    void Map::addMal(int row, int col, string** mal);
+
+public:
+    Map();
+    Map(Station mapPlayerLocation[7][7]);
+    void moveMal(int previRow, int previCol, int row, int col, string** mal);
+    void movePlayerLocation(int previRow, int previCol, int row, int col);
+    void UpdatePlayerLocation(int previRow, int previCol, int row, int col);
+    string printMap();
+    
+};
+
+#endif
