4e41d8a26cad4cbbfb435b3eba54e7ec7c2bba59
suhan0906 <201405suhan@gmail.com>
2023-04-24 19:35:15 +0800

    Empty .h and .cpp files for functions used in main

---
+++ Functions.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <string>
+#include "Functions.h"
+using namespace std;
---
+++ Functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+#endif
