6531e0eaa0aec215060acf7ab9b9743ffdea3502
suhan0906 <201405suhan@gmail.com>
2023-04-25 02:20:32 +0800

    Station struct used in Map Class

---
+++ Station.h
@@ -0,0 +1,13 @@
+#ifndef STATION_H
+#define STATION_H
+
+struct Station {
+    bool Playerone_first;
+    bool Playerone_second;
+    bool Playerone_third;
+    bool Playertwo_first;
+    bool Playertwo_second;
+    bool Playertwo_third;
+};
+
+#endif
