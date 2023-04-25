0ad6080f02d550ca88765046361834125d4b59ab
YumKim0113 <125568011+YumKim0113@users.noreply.github.com>
2023-04-26 01:54:04 +0800

    Update Ticket Display.cpp
    Adding another display for more directly represent possible moves

--- Ticket Display.cpp
+++ Ticket Display.cpp
@@ -19,7 +19,14 @@ void displayTicket(const std::string& type)
         std::cout << CYAN << "  /         |         |         |         |         |         |         |         |" << RESET << "\n";
         std::cout << CYAN << " /    P    /    L    /    A    /    T    /    I    /    N    /    U    /    M    /" << RESET << "\n";
         std::cout << CYAN << "/_________/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        moves=5;
+        std::cout << CYAN << "┌─────────┐\n";
+        std::cout << CYAN << "│ x     x │\n";
+        std::cout << CYAN << "│         │\n";
+        std::cout << CYAN << "│         │\n";
+        std::cout << CYAN << "│         │\n";
+        std::cout << CYAN << "│ x     x │\n";
+        std::cout << CYAN << "└─────────┘\n\n";
+        moves=4;
     }
     
     else if (type == "gold")
@@ -28,7 +35,14 @@ void displayTicket(const std::string& type)
         std::cout << YELLOW << "  /         |         |         |         |" << RESET << "\n";
         std::cout << YELLOW << " /    G    /    O    /    L    /    D    /" << RESET << "\n";
         std::cout << YELLOW << "/_________/_________/_________/_________/" << RESET << "\n";
-        moves=4;
+        std::cout << YELLOW << "┌─────────┐\n";
+        std::cout << YELLOW << "│ x       │\n";
+        std::cout << YELLOW << "│         │\n";
+        std::cout << YELLOW << "│    x    │\n";
+        std::cout << YELLOW << "│         │\n";
+        std::cout << YELLOW << "│       x │\n";
+        std::cout << YELLOW << "└─────────┘\n\n";
+        moves=3;
     }
     else if (type == "silver")
     {
@@ -36,15 +50,29 @@ void displayTicket(const std::string& type)
         std::cout << GRAY << "  /         |         |         |         |         |         |" << RESET << "\n";
         std::cout << GRAY << " /    S    /    I    /    L    /    V    /    E    /    R    /" << RESET << "\n";
         std::cout << GRAY << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        moves=3;
+        std::cout << GRAY << "┌─────────┐\n";
+        std::cout << GRAY << "│ x       │\n";
+        std::cout << GRAY << "│         │\n";
+        std::cout << GRAY << "│         │\n";
+        std::cout << GRAY << "│         │\n";
+        std::cout << GRAY << "│       x │\n";
+        std::cout << GRAY << "└─────────┘\n\n";
+        moves=2;
     }
     else if (type == "bronze")
     {
         std::cout << BROWN << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
         std::cout << BROWN << "  /         |         |         |         |         |         |" << RESET << "\n";
         std::cout << BROWN << " /    B    /    R    /    O    /    N    /    Z    /    E    /" << RESET << "\n";
-        std::cout << BROWN << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        moves=2;
+        std::cout << BROWN << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
+        std::cout << BROWN << "┌─────────┐\n";
+        std::cout << BROWN << "│         │\n";
+        std::cout << BROWN << "│         │\n";
+        std::cout << BROWN << "│    x    │\n";
+        std::cout << BROWN << "│         │\n";
+        std::cout << BROWN << "│         │\n";
+        std::cout << BROWN << "└─────────┘\n\n";
+        moves=1;
     }
     else if (type == "diamond")
     {
@@ -52,14 +80,28 @@ void displayTicket(const std::string& type)
         std::cout << BLUE << "  /         |         |         |         |         |        |          |" << RESET << "\n";
         std::cout << BLUE << " /    D    /    I    /    A    /    M    /    O    /    N   /     D    /" << RESET << "\n";
         std::cout << BLUE << "/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        moves=1;
+        std::cout << BLUE << "┌─────────┐\n";
+        std::cout << BLUE << "│ x     x │\n";
+        std::cout << BLUE << "│         │\n";
+        std::cout << BLUE << "│    x    │\n";
+        std::cout << BLUE << "│         │\n";
+        std::cout << BLUE << "│ x     x │\n";
+        std::cout << BLUE << "└─────────┘\n\n";
+        moves=5;
     }
     else if (type == "secret")
     {
     std::cout << MAGENTA << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
     std::cout << MAGENTA << "  /         |         |         |         |         |         |" << RESET << "\n";
     std::cout << MAGENTA << " /    S    /    E    /    C    /    R    /    E    /    T    /" << RESET << "\n";
-    std::cout << MAGENTA << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
+    std::cout << MAGENTA << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
+    std::cout << MAGENTA <<  "┌─────────┐\n";
+    std::cout << MAGENTA <<  "│ ?     ? │\n";
+    std::cout << MAGENTA <<  "│         │\n";
+    std::cout << MAGENTA <<  "│    ?    │\n";
+    std::cout << MAGENTA <<  "│         │\n";
+    std::cout << MAGENTA <<  "│ ?     ? │\n";
+    std::cout << MAGENTA <<  "└─────────┘\n\n";
         moves=-1;
     }
 
