9d0fd0d77fc7a5740e6929df97df82e177294993
suhan0906 <201405suhan@gmail.com>
2023-04-26 20:51:08 +0800

    Added row&col conversion function in map class

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <map>
 #include "Map.h"
 #include "Station.h"
 using namespace std;
@@ -33,6 +35,47 @@ Map::Map(Station mapPlayerLocation[7][7]) {
 
 Station Map::getPlayerLocation(int row, int col){
     return mapPlayerLocation[row][col];
+}
+
+int* mapPlayerLocation_to_mapChart(int row, int col){
+    map<array<int, 2>, array<int, 2>> rowColMatch;
+    
+    rowColMatch[{{6,0}}] = {{54,2}}; //HKU
+    rowColMatch[{{1, 0}}] = {{0, 1}};
+
+    int IndexInMapDisplay[29][2] =
+    
+    {
+        { 2, 2 }, //Kowloon
+        { 2, 16 }, //Symphony of Lights
+        { 2, 30 }, //Mong Kok
+        { 2, 44 },//Ladies' Market
+        { 2, 58 },//Soho
+        { 2, 72 },//North Point
+        { 10, 14 }, //Wong Tai Sin
+        { 10, 60 }, //Night Market
+        { 12, 2 }, //HKSM
+        { 12, 72 }, //Golden Computer Arcade
+        { 19, 26 }, //Elements Mall 
+        { 19, 48 }, //Promenade
+        { 23, 2 }, //HKMOH
+        { 23, 72 }, //Langham Place
+        { 28, 37 }, //Tsim Sa Tsui
+        { 33, 2 }, //HKMOA
+        { 33, 72 }, //Avenue of Stars
+        { 37, 26 }, //Clock Tower
+        { 37, 48 }, //Lan Kwai Fong
+        { 44, 2 }, //Repluse Bay
+        { 44, 72 }, //Tai O
+        { 46, 14 }, //Victoria Peak
+        { 46, 60 }, //Tai Kwun
+        { 54, 2 }, //HKU
+        { 54, 16 }, //Disneyland
+        { 54, 30 }, //Ocean Park
+        { 54, 44 }, //Man Mo
+        { 54, 58 }, //Western Market
+        { 54, 72 }, //Admiralty
+    };
 }
 
 void Map::removeMal(int row, int col){
