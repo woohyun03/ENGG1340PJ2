4451afe19999aed44639f7b659b6a4875141a043
Woohyun Choi <86250072+woohyun03@users.noreply.github.com>
2023-04-26 17:42:53 +0800

    Changed coding style and return value

--- Ticket Display.cpp
+++ Ticket Display.cpp
@@ -9,100 +9,102 @@
 #define CYAN "\033[36m"
 #define GRAY "\033[37m"
 #define BROWN "\033[38;2;205;127;50m"
+using namespace std;
 
-void displayTicket(const std::string& type)
+void displayTicket(const string& type)
 {
     int moves = 0; // Initialize moves to 0
     if (type == "platinum")
     {
-        std::cout << CYAN << "   _________ _________ _________ _________ _________ _________ _________ _________" << RESET << "\n";
-        std::cout << CYAN << "  /         |         |         |         |         |         |         |         |" << RESET << "\n";
-        std::cout << CYAN << " /    P    /    L    /    A    /    T    /    I    /    N    /    U    /    M    /" << RESET << "\n";
-        std::cout << CYAN << "/_________/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        std::cout << CYAN << "┌─────────┐\n";
-        std::cout << CYAN << "│ x     x │\n";
-        std::cout << CYAN << "│         │\n";
-        std::cout << CYAN << "│         │\n";
-        std::cout << CYAN << "│         │\n";
-        std::cout << CYAN << "│ x     x │\n";
-        std::cout << CYAN << "└─────────┘\n\n";
+        cout << CYAN << "   _________ _________ _________ _________ _________ _________ _________ _________" << RESET << "\n";
+        cout << CYAN << "  /         |         |         |         |         |         |         |         |" << RESET << "\n";
+        cout << CYAN << " /    P    /    L    /    A    /    T    /    I    /    N    /    U    /    M    /" << RESET << "\n";
+        cout << CYAN << "/_________/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
+        cout << CYAN << "┌─────────┐\n";
+        cout << CYAN << "│ x     x │\n";
+        cout << CYAN << "│         │\n";
+        cout << CYAN << "│         │\n";
+        cout << CYAN << "│         │\n";
+        cout << CYAN << "│ x     x │\n";
+        cout << CYAN << "└─────────┘\n\n";
         moves=4;
     }
     
     else if (type == "gold")
     {
-        std::cout << YELLOW << "   _________ _________ _________ _________ " << RESET << "\n";
-        std::cout << YELLOW << "  /         |         |         |         |" << RESET << "\n";
-        std::cout << YELLOW << " /    G    /    O    /    L    /    D    /" << RESET << "\n";
-        std::cout << YELLOW << "/_________/_________/_________/_________/" << RESET << "\n";
-        std::cout << YELLOW << "┌─────────┐\n";
-        std::cout << YELLOW << "│ x       │\n";
-        std::cout << YELLOW << "│         │\n";
-        std::cout << YELLOW << "│    x    │\n";
-        std::cout << YELLOW << "│         │\n";
-        std::cout << YELLOW << "│       x │\n";
-        std::cout << YELLOW << "└─────────┘\n\n";
+        cout << YELLOW << "   _________ _________ _________ _________ " << RESET << "\n";
+        cout << YELLOW << "  /         |         |         |         |" << RESET << "\n";
+        cout << YELLOW << " /    G    /    O    /    L    /    D    /" << RESET << "\n";
+        cout << YELLOW << "/_________/_________/_________/_________/" << RESET << "\n";
+        cout << YELLOW << "┌─────────┐\n";
+        cout << YELLOW << "│ x       │\n";
+        cout << YELLOW << "│         │\n";
+        cout << YELLOW << "│    x    │\n";
+        cout << YELLOW << "│         │\n";
+        cout << YELLOW << "│       x │\n";
+        cout << YELLOW << "└─────────┘\n\n";
         moves=3;
     }
     else if (type == "silver")
     {
-        std::cout << GRAY << "   _________ _________ _________ _________ _________ _________ " << RESET << "\n";
-        std::cout << GRAY << "  /         |         |         |         |         |         |" << RESET << "\n";
-        std::cout << GRAY << " /    S    /    I    /    L    /    V    /    E    /    R    /" << RESET << "\n";
-        std::cout << GRAY << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        std::cout << GRAY << "┌─────────┐\n";
-        std::cout << GRAY << "│ x       │\n";
-        std::cout << GRAY << "│         │\n";
-        std::cout << GRAY << "│         │\n";
-        std::cout << GRAY << "│         │\n";
-        std::cout << GRAY << "│       x │\n";
-        std::cout << GRAY << "└─────────┘\n\n";
+        cout << GRAY << "   _________ _________ _________ _________ _________ _________ " << RESET << "\n";
+        cout << GRAY << "  /         |         |         |         |         |         |" << RESET << "\n";
+        cout << GRAY << " /    S    /    I    /    L    /    V    /    E    /    R    /" << RESET << "\n";
+        cout << GRAY << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
+        cout << GRAY << "┌─────────┐\n";
+        cout << GRAY << "│ x       │\n";
+        cout << GRAY << "│         │\n";
+        cout << GRAY << "│         │\n";
+        cout << GRAY << "│         │\n";
+        cout << GRAY << "│       x │\n";
+        cout << GRAY << "└─────────┘\n\n";
         moves=2;
     }
     else if (type == "bronze")
     {
-        std::cout << BROWN << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
-        std::cout << BROWN << "  /         |         |         |         |         |         |" << RESET << "\n";
-        std::cout << BROWN << " /    B    /    R    /    O    /    N    /    Z    /    E    /" << RESET << "\n";
-        std::cout << BROWN << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
-        std::cout << BROWN << "┌─────────┐\n";
-        std::cout << BROWN << "│         │\n";
-        std::cout << BROWN << "│         │\n";
-        std::cout << BROWN << "│    x    │\n";
-        std::cout << BROWN << "│         │\n";
-        std::cout << BROWN << "│         │\n";
-        std::cout << BROWN << "└─────────┘\n\n";
+        cout << BROWN << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
+        cout << BROWN << "  /         |         |         |         |         |         |" << RESET << "\n";
+        cout << BROWN << " /    B    /    R    /    O    /    N    /    Z    /    E    /" << RESET << "\n";
+        cout << BROWN << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
+        cout << BROWN << "┌─────────┐\n";
+        cout << BROWN << "│         │\n";
+        cout << BROWN << "│         │\n";
+        cout << BROWN << "│    x    │\n";
+        cout << BROWN << "│         │\n";
+        cout << BROWN << "│         │\n";
+        cout << BROWN << "└─────────┘\n\n";
         moves=1;
     }
     else if (type == "diamond")
     {
-        std::cout << BLUE << "   _________ _________ _________ _________ _________ _________ _________" << RESET << "\n";
-        std::cout << BLUE << "  /         |         |         |         |         |        |          |" << RESET << "\n";
-        std::cout << BLUE << " /    D    /    I    /    A    /    M    /    O    /    N   /     D    /" << RESET << "\n";
-        std::cout << BLUE << "/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
-        std::cout << BLUE << "┌─────────┐\n";
-        std::cout << BLUE << "│ x     x │\n";
-        std::cout << BLUE << "│         │\n";
-        std::cout << BLUE << "│    x    │\n";
-        std::cout << BLUE << "│         │\n";
-        std::cout << BLUE << "│ x     x │\n";
-        std::cout << BLUE << "└─────────┘\n\n";
+        cout << BLUE << "   _________ _________ _________ _________ _________ _________ _________" << RESET << "\n";
+        cout << BLUE << "  /         |         |         |         |         |        |          |" << RESET << "\n";
+        cout << BLUE << " /    D    /    I    /    A    /    M    /    O    /    N   /     D    /" << RESET << "\n";
+        cout << BLUE << "/_________/_________/_________/_________/_________/_________/_________/" << RESET << "\n";
+        cout << BLUE << "┌─────────┐\n";
+        cout << BLUE << "│ x     x │\n";
+        cout << BLUE << "│         │\n";
+        cout << BLUE << "│    x    │\n";
+        cout << BLUE << "│         │\n";
+        cout << BLUE << "│ x     x │\n";
+        cout << BLUE << "└─────────┘\n\n";
         moves=5;
     }
     else if (type == "secret")
     {
-    std::cout << MAGENTA << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
-    std::cout << MAGENTA << "  /         |         |         |         |         |         |" << RESET << "\n";
-    std::cout << MAGENTA << " /    S    /    E    /    C    /    R    /    E    /    T    /" << RESET << "\n";
-    std::cout << MAGENTA << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
-    std::cout << MAGENTA <<  "┌─────────┐\n";
-    std::cout << MAGENTA <<  "│ ?     ? │\n";
-    std::cout << MAGENTA <<  "│         │\n";
-    std::cout << MAGENTA <<  "│    ?    │\n";
-    std::cout << MAGENTA <<  "│         │\n";
-    std::cout << MAGENTA <<  "│ ?     ? │\n";
-    std::cout << MAGENTA <<  "└─────────┘\n\n";
+        cout << MAGENTA << "   _________ _________ _________ _________ _________ _________" << RESET << "\n";
+        cout << MAGENTA << "  /         |         |         |         |         |         |" << RESET << "\n";
+        cout << MAGENTA << " /    S    /    E    /    C    /    R    /    E    /    T    /" << RESET << "\n";
+        cout << MAGENTA << "/_________/_________/_________/_________/_________/_________/" << RESET << "\n"
+        cout << MAGENTA <<  "┌─────────┐\n";
+        cout << MAGENTA <<  "│ ?     ? │\n";
+        cout << MAGENTA <<  "│         │\n";
+        cout << MAGENTA <<  "│    ?    │\n";
+        cout << MAGENTA <<  "│         │\n";
+        cout << MAGENTA <<  "│ ?     ? │\n";
+        cout << MAGENTA <<  "└─────────┘\n\n";
         moves=-1;
     }
+    return moves;
 
 }
