7913709dd53b7e7d7c054062b0b62045521ab03a
Woohyun Choi <86250072+woohyun03@users.noreply.github.com>
2023-04-26 19:11:44 +0800

    Changed return value from str to int

--- TicketProb.cpp
+++ TicketProb.cpp
@@ -4,15 +4,15 @@ using namepace std;
 string getTicket(){
     int randNum = rand() % 16 + 1;
     if (randNum == 1)
-        return "Diamond";
+        return 5; //diamond
     else if (randNum <= 2)
-        return "Platinum";
+        return 4; //platinum
     else if (randNum <= 6)
-        return "Gold";
+        return 3; //gold
     else if (randNum <= 8)
-        return "Silver";
+        return 2; //silver
     else if (randNum <= 12)
-        return "Bronze";
+        return 1; //bronze
     else if (randNum <= 16)
-        return "Secret";
+        return -1; //secret
 }
