be22c15f05524115af347d5b93f2aef4b669dcf7
Woohyun Choi <86250072+woohyun03@users.noreply.github.com>
2023-04-26 17:48:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
-
-string getTicket(){
-    int randNum = rand() % 16 + 1;
-    if (randNum == 1)
-        return "Diamond";
-    else if (randNum <= 2)
-        return "Platinum";
-    else if (randNum <= 6)
-        return "Gold";
-    else if (randNum <= 8)
-        return "Silver";
-    else if (randNum <= 12)
-        return "Bronze";
-    else if (randNum <= 16)
-        return "Secret";
-}
 
 int main(){
     cout << "=== Korean Traditional Yut Game (MTR EDITION) ===" << endl;
