a9445de1cad4a77c99d695c2a25f0e16399f81c8
Woohyun Choi <86250072+woohyun03@users.noreply.github.com>
2023-04-26 21:47:14 +0800

    Updated rowColMatch (all places)

--- Map.cpp
+++ Map.cpp
@@ -41,6 +41,40 @@ int* mapPlayerLocation_to_mapChart(int row, int col){
     map<array<int, 2>, array<int, 2>> rowColMatch;
     
     rowColMatch[{{6,0}}] = {{54,2}}; //HKU
+    rowColMatch[{{6,1}}] = {{54,16}}; //Disneyland
+    rowColMatch[{{6,2}}] = {{54,30}}; //Ocean Park
+    rowColMatch[{{6,4}}] = {{54,44}}; //Man Mo
+    rowColMatch[{{6,5}}] = {{54,58}}; //Western Market
+    rowColMatch[{{6,6}}] = {{54,72}}; //Admiralty
+    
+    rowColMatch[{{5,0}}] = {{44,2}}; //Repulse Bay
+    rowColMatch[{{5,1}}] = {{44,14}}; //Victoria Park
+    rowColMatch[{{5,5}}] = {{44,60}}; //Tai Kwun
+    rowColMatch[{{5,6}}] = {{44,72}}; //Tai O
+    
+    rowColMatch[{{4,0}}] = {{33,2}}; //HKMOA
+    rowColMatch[{{4,2}}] = {{37,26}}; //Clock Tower
+    rowColMatch[{{4,4}}] = {{37,48}}; //Lan Kwai Fong
+    rowColMatch[{{4,6}}] = {{33,72}}; //Avenue of Stars
+    
+    rowColMatch[{{3,3}}] = {{28,37}}; //Tsim Sha Tsui 
+    
+    rowColMatch[{{2,0}}] = {{23,2}}; //HKMOH
+    rowColMatch[{{2,2}}] = {{19,26}}; //Elements Mall
+    rowColMatch[{{2,4}}] = {{19,48}}; //Promenade
+    rowColMatch[{{2,6}}] = {{23,72}}; //Lagham Place
+    
+    rowColMatch[{{1,0}}] = {{12,2}}; //HKSM
+    rowColMatch[{{1,1}}] = {{10,14}}; //Wong Tai Sin
+    rowColMatch[{{1,5}}] = {{10,60}}; //Night Market
+    rowColMatch[{{1,6}}] = {{12,72}}; //Golden Computer Arcade
+    
+    rowColMatch[{{0,0}}] = {{2,2}}; //Kowloon
+    rowColMatch[{{0,1}}] = {{2,16}}; //Sympony of Lights
+    rowColMatch[{{0,2}}] = {{2,30}}; //Mong Kok
+    rowColMatch[{{0,4}}] = {{2,44}}; //Ladies' Market
+    rowColMatch[{{0,5}}] = {{2,58}}; //Soho
+    rowColMatch[{{0,6}}] = {{2,72}}; //North Point
     rowColMatch[{{1, 0}}] = {{0, 1}};
 
     int IndexInMapDisplay[29][2] =
