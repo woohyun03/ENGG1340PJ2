cc8e28b2958a8eb4bdebfb8367e91a2c8e86e51b
suhan0906 <201405suhan@gmail.com>
2023-04-26 17:02:08 +0800

    Added killMal function

--- Functions.cpp
+++ Functions.cpp
@@ -1,4 +1,53 @@
 #include <iostream>
 #include <string>
 #include "Functions.h"
+#include "Map.h"
+#include "Player.h"
+#include "Station.h"
 using namespace std;
+
+void killMal(Map &gameMap, int killerPlayerNum, int row, int col){
+    bool killFirst = false;
+    bool killSecond = false;
+    bool killThird = false;
+
+    if (killerPlayerNum == 1){
+        if (gameMap.getPlayerLocation(row, col).Playertwo_first){
+            killFirst = true;
+        }
+        if (gameMap.getPlayerLocation(row, col).Playertwo_second){
+            killSecond = true;
+        }
+        if (gameMap.getPlayerLocation(row, col).Playertwo_third){
+            killThird = true;
+        }
+    } else if (killerPlayerNum == 2){
+        if (gameMap.getPlayerLocation(row, col).Playerone_first){
+            killFirst = true;
+        }
+        if (gameMap.getPlayerLocation(row, col).Playerone_second){
+            killSecond = true;
+        }
+        if (gameMap.getPlayerLocation(row, col).Playerone_third){
+            killThird = true;
+        }
+    }
+
+    cout << "You killed the opponent's mal number(s):";
+
+    if (killFirst){
+        gameMap.removePlayerLocation(row, col, killerPlayerNum + 1, 1);
+        cout << " 1";
+    }
+    if (killSecond){
+        gameMap.removePlayerLocation(row, col, killerPlayerNum + 1, 2);
+        cout << " 2";
+    }
+    if (killThird){
+        gameMap.removePlayerLocation(row, col, killerPlayerNum + 1, 3);
+        cout << " 3";
+    }
+
+    cout << "." << endl;
+}
+
--- Functions.h
+++ Functions.h
@@ -2,8 +2,11 @@
 #define FUNCTIONS_H
 #include <iostream>
 #include <string>
+#include "Map.h"
+#include "Player.h"
+#include "Station.h"
 using namespace std;
 
-
+void killMal(Map &gameMap, int playerNum, int killerMalNum, int newRow, int newCol);
 
 #endif
