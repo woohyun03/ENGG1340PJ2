0a51412b1bb3a302e35389e1ee3f9ace3defcdb6
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2023-04-29 04:57:40 +0800

    Match first parts with classes and functions

--- main.cpp
+++ main.cpp
@@ -1,43 +1,73 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Functions.h"
+#include "Map.h"
+#include "Player.h"
+#include "Station.h"
+#include "Mal.h"
 using namespace std;
 
 int main(){
-    cout << "Welcome to ... Korean Traditional Yut Game (MTR EDITION) !!!" << endl;
+    Map gameMap;
+    Player player1;
+    Player player2;
     int counter = -1;
     int turnShower;
+    int TicketResult;
+    string input;
+    int malSelect;
+
+    cout << "Welcome to ... Korean Traditional Yut Game (MTR EDITION) !!!" << endl;
+    cout << "Enter player 1's name: ";
+    cin >> input;
+    player1.setName(input);
+    cout << "Enter player 2's name: ";
+    cin >> input;
+    player2.setName(input);
 
     while (true) {
-        counter = counter + 1;
-        cout << "Continue the Game? ('q' for exit): ";
-        string input;
-        getline(cin, input);
 
-        if (input == "q" || input == "Q") {
-            cout << "Exit the Game." << endl;
-            break;
+        counter = counter + 1;
+        if (counter != -1){
+            cout << "Continue the Game? ('q' for exit / press Enter if you want to continue): ";
+            cin >> input;
+            if (input == "q" || input == "Q") {
+                cout << "Exit the Game." << endl;
+                break;
+            }
         }
-        int malSelect;
+
+
         if(counter % 2 == 0){
-            cout << "Which mal would you like to move? (1,2,3)" << endl;
-            turnShower = 1;
+            cout << "It is Player 1's turn";
+            turnShower = 0;
         }
         else{
-            cout << "Which mal would you like to move? (4,5,6)" << endl;
-            turnShower = 2;
+            cout << "It is Player 2's turn";
+            turnShower = 1;
         }
+
+        vector<string> malVector = askMalMovement(turnShower, player1, player2);
         cin >> malSelect;
-        cout << "Current Position of " << "mal number " << malSelect << " is" << PositionArray[malSelect] << endl;
+
+        if (turnShower == 0){
+            cout << "Current Position of " << malVector[malSelect] << " is" << gameMap.mapPlayerLocation_to_station(player1.getRow(malSelect),player1.getCol(malSelect)) << endl;
+        } else {
+            cout << "Current Position of " << malVector[malSelect] << " is" << gameMap.mapPlayerLocation_to_station(player2.getRow(malSelect),player2.getCol(malSelect)) << endl;
+        }
+
         TicketResult = getTicket();
         cout << "Your ticket is..." << TicketResult << endl;
 
         //move mal and save other info (function to be added)
         //display map (functions to be added)
 
-        cout << "You have arrived to..." << //place explantion functio << endl;
+        cout << "You have arrived to..." << /*place explantion functio << endl*/;
 
         for(int i = 0, i < 5; ++i){
             if(currentPosition == PositionArray[i]){
-                if(//catched opponent's mal){
+                if(/*catched opponent's mal*/){
                     cout << "You have Killed the oppoent's mal number " << MalArray[i] << endl;
                     PositionArray[i] = 0;
                 }
@@ -47,7 +77,7 @@ int main(){
                 }
             }
         }
-        if(//winning condition){
+        if(/*winning condition*/){
             break
         }
         cout << "Your turn has ended, it is player " << turnShower << "'s turn" << /n << endl;
