3ec819f0c4e3e1e08e6ef06e402c3a6c82bcef9c
YumKim0113 <125568011+YumKim0113@users.noreply.github.com>
2023-05-02 17:25:26 +0800

    Update Mal.h
    Adding comments in Map.h

--- Mal.h
+++ Mal.h
@@ -1,14 +1,15 @@
 #ifndef MAL_H
 #define MAL_H
 
+// Struct representing a single game piece (Mal)
 struct Mal {
-    int row;
-    int column;
-    int previousRow;
-    int previousColumn;
-    bool can_finish;
-    bool finished;
-    bool carried;
+    int row;                // The current row of the Mal on the game board
+    int column;             // The current column of the Mal on the game board
+    int previousRow;        // The previous row of the Mal on the game board
+    int previousColumn;     // The previous column of the Mal on the game board
+    bool can_finish;        // Indicates if the Mal can finish (reach the end point) in the current move
+    bool finished;          // Indicates if the Mal has finished (reached the end point)
+    bool carried;           // Indicates if the Mal is being carried by a player
 };
 
 #endif
