f4024170dd79bd7c880c401d6b5be33c96071178
EunseopShim <110234014+EunseopShim@users.noreply.github.com>
2023-05-02 19:45:13 +0800

    Comment on Player.h

--- Player.h
+++ Player.h
@@ -9,34 +9,34 @@ using namespace std;
 
 class Player {
   private:
-    string name;
-    vector<Mal> Mals;
-    void only_six_positions_in_edges(Mal &playerMal);
-    void moveStraight(Mal &playerMal, int &moveNum);
-    void corner_and_diagonal(Mal &playerMal, int &moveNum);
-    void center(Mal &playerMal, int &moveNum);
-    void Back(Mal &playerMal);
+    string name;  // Declare name 
+    vector<Mal> Mals; // Declare vector Mals in Mal class
+    void only_six_positions_in_edges(Mal &playerMal); //Declare function when mal is in edges
+    void moveStraight(Mal &playerMal, int &moveNum); //Declare function for moving straight
+    void corner_and_diagonal(Mal &playerMal, int &moveNum); // Declare a function for corner and diagonal
+    void center(Mal &playerMal, int &moveNum); // Declare a function when mal is in center
+    void Back(Mal &playerMal); //Declare a function when mal move backward
 
   public:
-    Player();
-    Player(string name, int first1, int first2, int second1, int second2, int third1, int third2);
-    string getName();
-    void setName(string name);
-    int getRow(int malNum);
-    int getCol(int malNum);
-    void setRowCol(int malNum, int row, int col);
-    int getPreviousRow(int malNum);
-    int getPreviousCol(int malNum);
-    void setPreviousRowCol(int malNum, int row, int col);
-    Mal getMal(int malNum);
-    void setMal(int malNum, int row, int col, bool can_finish, bool finished, bool carried);
-    void set_cannot_finish(int malNum);
-    bool win();
-    void check_Carried();
-    void moveMal(int malNum, int moveNum);
-    void newRowCol (Mal &playerMal, int &moveNum);
-    void can_finish(Mal &playerMal);
-    void finish(Mal &playerMal, int &moveNum);
+    Player(); //Declare constructor
+    Player(string name, int first1, int first2, int second1, int second2, int third1, int third2); // Specify the constructor
+    string getName(); //Function for getting name
+    void setName(string name); //Function for setting a name
+    int getRow(int malNum); //Function for getRow
+    int getCol(int malNum);// Function for getting column
+    void setRowCol(int malNum, int row, int col); //Function for setting col and row for given mal
+    int getPreviousRow(int malNum); //Function for getting previous row
+    int getPreviousCol(int malNum); //Fucntion for getting previous column
+    void setPreviousRowCol(int malNum, int row, int col); // Function for setting previous row and column
+    Mal getMal(int malNum); //Function for getting a mal return the mal type.
+    void setMal(int malNum, int row, int col, bool can_finish, bool finished, bool carried); //Function for set a mal
+    void set_cannot_finish(int malNum); // Function when unable to finish
+    bool win(); //Function for distinguishing end of game
+    void check_Carried(); //Function for checking carried mals
+    void moveMal(int malNum, int moveNum); //Function for moving mal
+    void newRowCol (Mal &playerMal, int &moveNum); //Function for setting new location of mal after moving
+    void can_finish(Mal &playerMal); //Function when able to finish
+    void finish(Mal &playerMal, int &moveNum); //Function to finish 
 };
 
 #endif
