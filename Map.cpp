573273480fc3666ee97e22761806d404a5126b65
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2023-05-02 22:19:08 +0800

    fixed minor error

--- Map.cpp
+++ Map.cpp
@@ -57,35 +57,35 @@ Station Map::getPlayerLocation(int row, int col){
 string Map::mapPlayerLocation_to_station(int row, int col){
     typedef pair<int, int> rowCol;
     map< rowCol, string > rowColMatch;
-    rowColMatch.insert(make_pair(make_pair(6,0), make_pair(54,2))); //HKU
-    rowColMatch.insert(make_pair(make_pair(6,1), make_pair(54,16))); //Disneyland
-    rowColMatch.insert(make_pair(make_pair(6,2), make_pair(54,30))); //Ocean Park
-    rowColMatch.insert(make_pair(make_pair(6,4), make_pair(54,44))); //Man Mo
-    rowColMatch.insert(make_pair(make_pair(6,5), make_pair(54,58))); //Western Market
-    rowColMatch.insert(make_pair(make_pair(6,6), make_pair(54,72))); //Admiralty
-    rowColMatch.insert(make_pair(make_pair(5,0), make_pair(44,2))); //Repulse Bay
-    rowColMatch.insert(make_pair(make_pair(5,1), make_pair(46,14))); //Victoria Park
-    rowColMatch.insert(make_pair(make_pair(5,5), make_pair(46,60))); //Tai Kwun
-    rowColMatch.insert(make_pair(make_pair(5,6), make_pair(44,72))); //Tai O
-    rowColMatch.insert(make_pair(make_pair(4,0), make_pair(33,2))); //HKMOA
-    rowColMatch.insert(make_pair(make_pair(4,2), make_pair(37,26))); //Clock Tower
-    rowColMatch.insert(make_pair(make_pair(4,4), make_pair(37,48))); //Lan Kwai Fong
-    rowColMatch.insert(make_pair(make_pair(4,6), make_pair(33,72))); //Avenue of Stars
-    rowColMatch.insert(make_pair(make_pair(3,3), make_pair(28,37))); //Tsim Sha Tsui
-    rowColMatch.insert(make_pair(make_pair(2,0), make_pair(23,2))); //HKMOH
-    rowColMatch.insert(make_pair(make_pair(2,2), make_pair(19,26))); //Elements Mall
-    rowColMatch.insert(make_pair(make_pair(2,4), make_pair(19,48))); //Promenade
-    rowColMatch.insert(make_pair(make_pair(2,6), make_pair(23,72))); //Lagham Place
-    rowColMatch.insert(make_pair(make_pair(1,0), make_pair(12,2))); //HKSM
-    rowColMatch.insert(make_pair(make_pair(1,1), make_pair(10,14))); //Wong Tai Sin
-    rowColMatch.insert(make_pair(make_pair(1,5), make_pair(10,60))); //Night Market
-    rowColMatch.insert(make_pair(make_pair(1,6), make_pair(12,72))); //Golden Computer Arcade
-    rowColMatch.insert(make_pair(make_pair(0,0), make_pair(2,2))); //Kowloon
-    rowColMatch.insert(make_pair(make_pair(0,1), make_pair(2,16))); //Sympony
-    rowColMatch.insert(make_pair(make_pair(0,2), make_pair(2,30))); //Mong Kok
-    rowColMatch.insert(make_pair(make_pair(0,4), make_pair(2,44))); //Ladies' Market
-    rowColMatch.insert(make_pair(make_pair(0,5), make_pair(2,58))); //Soho
-    rowColMatch.insert(make_pair(make_pair(0,6), make_pair(2,72))); //North Point
+    rowColMatch.insert(make_pair(make_pair(6, 0), "HKU"));
+    rowColMatch.insert(make_pair(make_pair(6, 1), "Disneyland"));
+    rowColMatch.insert(make_pair(make_pair(6, 2), "Ocean Park"));
+    rowColMatch.insert(make_pair(make_pair(6, 4), "Man Mo"));
+    rowColMatch.insert(make_pair(make_pair(6, 5), "Western Market"));
+    rowColMatch.insert(make_pair(make_pair(6, 6), "Admiralty"));
+    rowColMatch.insert(make_pair(make_pair(5, 0), "Repulse Bay"));
+    rowColMatch.insert(make_pair(make_pair(5, 1), "Victoria Park"));
+    rowColMatch.insert(make_pair(make_pair(5, 5), "Tai Kwun"));
+    rowColMatch.insert(make_pair(make_pair(5, 6), "Tai O"));
+    rowColMatch.insert(make_pair(make_pair(4, 0), "HKMOA"));
+    rowColMatch.insert(make_pair(make_pair(4, 2), "Clock Tower"));
+    rowColMatch.insert(make_pair(make_pair(4, 4), "Lan Kwai Fong"));
+    rowColMatch.insert(make_pair(make_pair(4, 6), "Avenue of Stars"));
+    rowColMatch.insert(make_pair(make_pair(3, 3), "Tsim Sha Tsui"));
+    rowColMatch.insert(make_pair(make_pair(2, 0), "HKMOH"));
+    rowColMatch.insert(make_pair(make_pair(2, 2), "Elements Mall"));
+    rowColMatch.insert(make_pair(make_pair(2, 4), "Promenade"));
+    rowColMatch.insert(make_pair(make_pair(2, 6), "Lagham Place"));
+    rowColMatch.insert(make_pair(make_pair(1, 0), "HKSM"));
+    rowColMatch.insert(make_pair(make_pair(1, 1), "Wong Tai Sin"));
+    rowColMatch.insert(make_pair(make_pair(1, 5), "Night Market"));
+    rowColMatch.insert(make_pair(make_pair(1, 6), "Golden Computer Arcade"));
+    rowColMatch.insert(make_pair(make_pair(0, 0), "Kowloon"));
+    rowColMatch.insert(make_pair(make_pair(0, 1), "Sympony of Lights"));
+    rowColMatch.insert(make_pair(make_pair(0, 2), "Mong Kok"));
+    rowColMatch.insert(make_pair(make_pair(0, 4), "Ladies' Market"));
+    rowColMatch.insert(make_pair(make_pair(0, 5), "Soho"));
+    rowColMatch.insert(make_pair(make_pair(0, 6), "North Point"));
 
     return rowColMatch[make_pair(row, col)];
 
