7e6a753048efd87e1e8eb2d6461a0a3deb0b9e7e
EunseopShim <jeffshim6857@gmail.com>
2023-05-03 20:19:00 +0800

    few correction

--- Mal.h
+++ Mal.h
@@ -7,7 +7,7 @@ struct Mal {
     int column;             // The current column of the Mal on the game board
     int previousRow;        // The previous row of the Mal on the game board
     int previousColumn;     // The previous column of the Mal on the game board
-    bool can_finish;        // Indicates if the Mal can finish (reach the end point) in the current move
+    bool can_finish;        // Indicates if the Mal has moved at least one time.
     bool finished;          // Indicates if the Mal has finished (reached the end point)
     bool carried;           // Indicates if the Mal is being carried by a player
 };
